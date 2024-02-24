8ad2a414a9023dddf0a240c41f11bbe61519c98c
Harmony <nellysoma2005@yahoo.com>
2024-02-24 15:37:31 +0100

    This is the commit message

--- src/main/java/com/example/demo/config/SecSecurityConfig.java
+++ src/main/java/com/example/demo/config/SecSecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  *
@@ -32,6 +33,39 @@ public class SecSecurityConfig {
     //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
     
     @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        http
+            .formLogin(formLogin -> formLogin
+                    .loginPage("/login")
+                    .failureUrl("/login-error.html"))
+            .logout(logout -> logout
+                    .logoutSuccessUrl("/index.html"))
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers(
+                            new AntPathRequestMatcher("/"),
+                            new AntPathRequestMatcher("/home.jsp"),
+                            new AntPathRequestMatcher("/index.html"),
+                            new AntPathRequestMatcher("/login"),
+                            new AntPathRequestMatcher("/css/**"),
+                            new AntPathRequestMatcher("/favicon.ico")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasRole("ADMIN")
+                    .requestMatchers(new AntPathRequestMatcher("/user/**")).hasRole("USER")
+                    .requestMatchers(new AntPathRequestMatcher("/shared/**")).hasAnyRole("USER","ADMIN")
+                    .anyRequest().authenticated())
+            .exceptionHandling(handling -> handling
+                    .accessDeniedPage("/403.html"));
+        return http.build();
+    }
+    
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("jim").password("{noop}demo").roles("ADMIN").build(),
+                User.withUsername("bob").password("{noop}demo").roles("USER").build(),
+                User.withUsername("ted").password("{noop}demo").roles("USER","ADMIN").build());
+    }
+    
+    /**@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		// @formatter:off
 		http
@@ -42,10 +76,10 @@ public class SecSecurityConfig {
 				.formLogin(withDefaults());
 		// @formatter:on
 		return http.build();
-	}
+	}**/
 
 	// @formatter:off
-	@Bean
+	/**@Bean
 	public InMemoryUserDetailsManager userDetailsService() {
 		UserDetails user = User.withDefaultPasswordEncoder()
 				.username("user")
@@ -53,7 +87,7 @@ public class SecSecurityConfig {
 				.roles("USER")
 				.build();
 		return new InMemoryUserDetailsManager(user);
-	}
+	}**/
 	// @formatter:on
     
     /**@Bean
